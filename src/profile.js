6655a0e3f14d70dbbef471dd4413bb26886b8013
darshini-codilar <darshini.r@codilar.com>
2021-12-07 11:25:13 +0530

    Search Bar added

--- src/profile.js
+++ src/profile.js
@@ -1,45 +1,56 @@
-import React ,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './profile.css';
 import Loader from './loader';
 
-export default function Profile ({state}) {
-    console.log(state);
-    const [user,setUser] =  useState(null);
-    var x=state;
+export default function Profile({ state }) {
+  const [search, setSearchTerm] = useState('');
+  console.log(search);
+  console.log(state);
+  const [user, setUser] = useState(null);
+  var x = state;
 
-    useEffect(() => { setTimeout(async() =>{
-        const response = await fetch('https://reqres.in/api/users?page='+x);
-        const data = await response.json();
-        setUser (data.data);
-        console.log(x);
-    
-    },500)
-   },[x])
+  useEffect(() => {
+    setTimeout(async () => {
+      const response = await fetch('https://reqres.in/api/users?page=' + x);
+      const data = await response.json();
+      setUser(data.data);
+      console.log(x);
 
-    return (
-      <>
-          <div className="cards">
-               
+    }, 500)
+  }, [x])
 
-  
-          {
-            user && user.map((el)=>{
-              return(
-                      
-                    <div className="card" key={el.id}>
-              <div className="avatar a1">
-                  <img src={el.avatar} alt="img"/>
+  return (
+    <>
+    <div className="search">
+      <input type="text" placeholder="search" onChange={val=>{setSearchTerm(val.target.value)}} ></input>
+    </div>
+      <div className="cards">
+
+        {
+          user && user.filter((el)=>{
+            if(search=== "")
+            {
+              return el;
+            }else if(el.first_name.toLowerCase().includes(search.toLocaleLowerCase())){
+              return el;
+            }
+          }).map((el) => {
+            return (
+
+              <div className="card" key={el.id}>
+                <div className="avatar a1">
+                  <img src={el.avatar} alt="img" />
+               </div>
+                <div className="text-name"> {el.first_name} {el.last_name}</div>
+                <div className="mid">{el.email}</div>
               </div>
-              <div className="text-name"> {el.first_name} {el.last_name}</div>
-              <div className="mid">{el.email}</div>
-          </div>
-  
-              )
-            })
-          }
-          {!user && [1,2,3,4,5,6].map((n)=> <Loader key={n}/>)}
-  </div> 
-      </>
-    );
-  
+
+            )
+          })
+        }
+        {!user && [1, 2, 3, 4, 5, 6].map((n) => <Loader key={n} />)}
+      </div>
+    </>
+  );
+
 }
